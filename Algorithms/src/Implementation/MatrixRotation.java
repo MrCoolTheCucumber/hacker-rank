5f786d073358a25bb622d66d7426e37d45a5b75e
Ruben <ruben@cwing.club>
2016-03-06 14:25:36 +0000

    Finished MatrixRotation

--- Algorithms/src/Implementation/MatrixRotation.java
+++ Algorithms/src/Implementation/MatrixRotation.java
@@ -1,8 +1,10 @@
 package Implementation;
 
 import java.awt.*;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+
 import static java.lang.System.in;
 
 public class MatrixRotation {
@@ -16,13 +18,20 @@ public class MatrixRotation {
 
         int[][] matrix = new int[m][n];
 
-        for(int i = 0; i < m; ++i) {
-            for(int j = 0; j < n; ++j) {
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
                 matrix[i][j] = sc.nextInt();
             }
         }
 
-        rotateMatrix(matrix, m, n);
+        Point[][] points = new Point[m][n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                points[i][j] = new Point(i, j);
+            }
+        }
+
+        rotateMatrix(matrix, r, points);
 
         printMatrix(matrix);
     }
@@ -36,19 +45,102 @@ public class MatrixRotation {
         }
     }
 
-    public static int getRotations(int m, int n, int r) {
-        return r % (((m - 1) + (n - 1)) * 2);
-    }
+    public static void rotateMatrix(int[][] matrix, int r, Point[][] points) {
+        int[][] map = deepCopy(matrix);
 
-    public static void rotateMatrix(int[][] matrix, int m, int n) {
-        int[][] map = matrix.clone();
+        ArrayList<ArrayList<Pair<Point, Point>>> rings = getRings(new ArrayList<>(), points);
 
-        //get rings, and remap rings?
+        for (ArrayList<Pair<Point, Point>> ring : rings) {
 
-        for(int i = 0; i < m; ++i) {
-            for(int j = 0; j < n; ++j) {
+            for (int i = 0; i < ring.size(); i++) {
+                Point p = ring.get(i).getK();
 
+                ring.get((i + r) % ring.size()).setV(p);
             }
         }
+
+        for (ArrayList<Pair<Point, Point>> ring : rings) {
+            for (Pair<Point, Point> pair : ring) {
+                matrix[pair.getK().x][pair.getK().y] = map[pair.getV().x][pair.getV().y];
+            }
+        }
+    }
+
+    public static ArrayList<ArrayList<Pair<Point, Point>>> getRings(
+            ArrayList<ArrayList<Pair<Point, Point>>> rings, Point[][] points) {
+
+        ArrayList<Pair<Point, Point>> ring = new ArrayList<>();
+
+        int i = 0;
+        //down
+        for (i = 0; i < points.length; ++i) {
+            ring.add(new Pair<>(points[i][0], null)); // ?
+        }
+        //right - skip outer corners
+        for (i = 1; i < points[points.length - 1].length - 1; ++i) {
+            ring.add(new Pair<>(points[points.length - 1][i], null));
+        }
+        //up
+        for (i = points.length - 1; i >= 0; --i) {
+            ring.add(new Pair<>(points[i][points[i].length - 1], null));
+        }
+        //left - skip outer corners
+        for (i = points[0].length - 2; i > 0; --i) {
+            ring.add(new Pair<>(points[0][i], null));
+        }
+
+        rings.add(ring);
+
+        if (points.length == 2 || points[0].length == 2) {
+            return rings;
+        } else {
+            return getRings(rings, removeOuterRing(points));
+        }
+    }
+
+    public static Point[][] removeOuterRing(Point[][] points) {
+        Point[][] trimmedMatrix = new Point[points.length - 2][points[0].length - 2];
+
+        for (int i = 0; i < points.length - 2; ++i) {
+            System.arraycopy(points[i + 1], 1, trimmedMatrix[i], 0, points[0].length - 2);
+        }
+
+        return trimmedMatrix;
+    }
+
+    public static class Pair<K, V> {
+
+        private final K element0;
+        private V element1;
+
+        public Pair(K k, V v) {
+            this.element0 = k;
+            this.element1 = v;
+        }
+
+        public K getK() {
+            return element0;
+        }
+
+        public void setV(V v) {
+            this.element1 = v;
+        }
+
+        public V getV() {
+            return element1;
+        }
+
+    }
+
+    public static int[][] deepCopy(int[][] original) {
+        if (original == null) {
+            return null;
+        }
+
+        final int[][] result = new int[original.length][];
+        for (int i = 0; i < original.length; i++) {
+            result[i] = Arrays.copyOf(original[i], original[i].length);
+        }
+        return result;
     }
 }
